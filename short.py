d6f9ad8651e90c1751f43ffc56e4d10b487fb972
Sergey <xxsmotur@gmail.com>
2019-11-18 11:27:49 +0500

    getting users and password from env. variables

--- short.py
+++ short.py
@@ -1,10 +1,16 @@
 import jenkins
-
+import os
 
 def create_job(server, path):
     server.get_job_config(path)
     server.build_job(path)
     server.delete_job(path)
 
-server = jenkins.Jenkins('http://localhost:8080')
+if 'JENKINS_SHORT_USER' not in os.environ:
+    raise Exception('user is not defined')
+if 'JENKINS_SHORT_USER' not in os.environ:
+    raise Exception('password is not defined')
+user = os.environ['JENKINS_SHORT_USER']
+password = os.environ['JENKINS_SHORT_PASSWORD']
+server = jenkins.Jenkins('http://localhost:8080', user, password)
 print (server.jobs_count())
